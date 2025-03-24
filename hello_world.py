910be53c0f52bde33573157fa500d76e80b764e3
VonHindenburg01 <alexandreha2101@hotmail.com>
2025-03-24 11:33:34 +0100

    e

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,14 @@ print("Hello dev")
 A = float(input("Input A: "))
 B = float(input("Input B: "))  
 operator = input("Operator (+, -, *, /): ")
+if operator == '+':
+    result=A+B 
+elif operator == '-':
+    result=A-B
+elif operator == '*':
+    result=A*B
+elif operator =='/':
+    result=A/B
+else:
+    result="Erreur: Division par zéro"
+       
